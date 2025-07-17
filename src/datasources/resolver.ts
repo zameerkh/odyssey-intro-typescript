58878a0ed7ddfa9391c45037c8f5a0d6db2ff2eb
zameerkh <zameer.kh@gmail.com>
2025-07-17 20:48:51 +1000

    Query by listing id

--- src/datasources/resolver.ts
+++ src/datasources/resolver.ts
@@ -6,5 +6,8 @@ export const resolvers: Resolvers<DataSourceContext> = {
     featuredListings: (_, __, { dataSources }) => {
       return dataSources.listingAPI.getFeaturedListings();
     },
+    listing: (_, { id }, { dataSources }) => {
+      return dataSources.listingAPI.getListing(id);
+    },
   },
 };
